65bda3c1abcdbf1321b7e6c007931e213d5364e1
Linus Walleij <linus.walleij@linaro.org>
2014-08-11 11:39:15 +0200

    Uglyhack for earlydebug printascii

--- init/main.c
+++ init/main.c
@@ -935,6 +935,13 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
 	debug_objects_early_init();
 	init_vmlinux_build_id();
 
+#if defined(CONFIG_ARM) && defined(CONFIG_DEBUG_LL)
+	{
+	  extern void printascii(char *);
+	  printascii("start_kernel\n");
+	}
+#endif
+
 	cgroup_init_early();
 
 	local_irq_disable();
--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2217,6 +2217,19 @@ int vprintk_store(int facility, int level,
 	r.info->caller_id = caller_id;
 	if (dev_info)
 		memcpy(&r.info->dev_info, dev_info, sizeof(r.info->dev_info));
+#if defined(CONFIG_ARM) && defined(CONFIG_DEBUG_LL)
+			{
+				static char foo[1024];
+				extern void printascii(char *);
+				int len;
+
+				len = text_len + trunc_msg_len;
+				memcpy(foo, &r.text_buf[0], len);
+				foo[len] = '\n';
+				foo[len+1] = '\0';
+				printascii(foo);
+			}
+#endif
 
 	/* A message without a trailing newline can be continued. */
 	if (!(flags & LOG_NEWLINE))
