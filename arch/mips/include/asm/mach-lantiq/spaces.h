384ee07b576343d57b5c2b356fef4a5928a0545e
John Crispin <john@phrozen.org>
2016-06-03 13:12:20 +0200

    arch: mips: increase io_space_limit
    this value comes from x86 and breaks some pci devices
    
    Signed-off-by: John Crispin <john@phrozen.org>

---
+++ arch/mips/include/asm/mach-lantiq/spaces.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __ASM_MACH_LANTIQ_SPACES_H_
+#define __ASM_MACH_LANTIQ_SPACES_H_
+
+#define IO_SPACE_LIMIT  0xffffffff
+
+#include <asm/mach-generic/spaces.h>
+#endif
