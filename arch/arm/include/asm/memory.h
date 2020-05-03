60bb889fd887159d5e17ba61139a66b21be482c1
afzal mohammed <afzal.mohd.ma@gmail.com>
2020-05-03 12:57:27 +0530

    ARM: move kernel image to vmalloc area
    VMALLOC_START = 0xf0000000
    VMALLOC_OFFSET = 0
    
    KIMAGE_OFFSET = 0xf0000000
    
    Not working
    
    Signed-off-by: afzal mohammed <afzal.mohd.ma@gmail.com>

--- arch/arm/include/asm/memory.h
+++ arch/arm/include/asm/memory.h
@@ -21,7 +21,7 @@
 
 /* PAGE_OFFSET - the virtual address of the start of the kernel image */
 #define PAGE_OFFSET		UL(CONFIG_PAGE_OFFSET)
-#define KIMAGE_OFFSET		PAGE_OFFSET
+#define KIMAGE_OFFSET		0xf0000000
 
 #ifdef CONFIG_MMU
 
