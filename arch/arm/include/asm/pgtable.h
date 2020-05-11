d30a6485de2dde6b59ec8122ce0b6c48f1b0445f
afzal mohammed <afzal.mohd.ma@gmail.com>
2020-05-11 16:25:54 +0530

    ARM: VMALLOC_OFFSET = 16MB, adjust vmalloc size accordingly
    Signed-off-by: afzal mohammed <afzal.mohd.ma@gmail.com>

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -39,8 +39,8 @@
  * The vmalloc() routines leaves a hole of 4kB between each vmalloced
  * area for the same reason. ;)
  */
-#define VMALLOC_OFFSET		0
-#define VMALLOC_START		((unsigned long)high_memory)
+#define VMALLOC_OFFSET		(16*1024*1024)
+#define VMALLOC_START		(((unsigned long )high_memory + VMALLOC_OFFSET) & ~(VMALLOC_OFFSET-1))
 #define VMALLOC_END		0xff800000UL
 
 #define LIBRARY_TEXT_START	0x0c000000
