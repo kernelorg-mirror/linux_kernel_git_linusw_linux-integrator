afzalmam/linux-kernel

ARM: skip kernel static mapping pmd clearin
Signed-off-by: afzal mohammed <afzal.mohd.ma@gmail.com>

arch/arm/mm/mmu.c
@@ -1341,9 +1341,11 @@ static void __init devicemaps_init(const struct machine_desc *mdesc)
 	early_trap_init(vectors);
 
 	/*
-	 * Clear page table except top pmd used by early fixmaps
+	 * Clear page table above VMALLOC_START except pmd's used by static
+	 * kernel & top pmd used by early fixmaps
 	 */
-	for (addr = VMALLOC_START; addr < (FIXADDR_TOP & PMD_MASK); addr += PMD_SIZE)
+	addr = __phys_to_kimg(round_up(__pa(KERNEL_END), PGDIR_SIZE));
+	for (; addr < (FIXADDR_TOP & PMD_MASK); addr += PMD_SIZE)
 		pmd_clear(pmd_off_k(addr));
 
 	/*

kernelorg-mirror/linux_kernel_git_linusw_linux-integrator

arch/arm/mm/mmu.c
@@ -1341,9 +1341,11 @@ static void __init devicemaps_init(const struct machine_desc *mdesc)
 	early_trap_init(vectors);
 
 	/*
-	 * Clear page table except top pmd used by early fixmaps
+	 * Clear page table above VMALLOC_START except pmd's used by static
+	 * kernel & top pmd used by early fixmaps
 	 */
-	for (addr = VMALLOC_START; addr < (FIXADDR_TOP & PMD_MASK); addr += PMD_SIZE)
+	addr = __phys_to_kimg(round_up(__pa(KERNEL_END), PMD_SIZE));
+	for (; addr < (FIXADDR_TOP & PMD_MASK); addr += PMD_SIZE)
 		pmd_clear(pmd_off_k(addr));
 
 	/*