cf80272f4c677cda56c3c934187043c597932d9d
Linus Walleij <linus.walleij@linaro.org>
2022-05-16 13:36:26 +0200

    ARM: kasan: Support discontiguous memory ranges
    The Kasan code currently assumes that each memory range is
    linerly mapped 1-to-1 from physical to virtual memory.
    So we assume that __va() applied from the start to the end
    of each memory range will return a contiguous address range.
    
    This does not work when using for example a kernel compiled
    into the vmalloc area: __va() will start to change in the
    middle of a physical address range as we are now mappin the
    kernel into a different place that the physical memory
    below and above it.
    
    Fix this by adding an inner loop to for_each_mem_range()
    in kasan_init() so that we walk the pages and line up
    those that are physically contiguos and call create_mapping()
    for each contiguous range.
    
    Add some comments and debug prints so it is clear to people
    reading the code what is going on in here.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mm/kasan_init.c
+++ arch/arm/mm/kasan_init.c
@@ -233,6 +233,8 @@ void __init kasan_init(void)
 	cpu_switch_mm(tmp_pgd_table, &init_mm);
 	local_flush_tlb_all();
 
+	pr_debug("clearing PGDs from %08llx-%08llx\n",
+		 (long long)KASAN_SHADOW_START, (long long)KASAN_SHADOW_END);
 	clear_pgds(KASAN_SHADOW_START, KASAN_SHADOW_END);
 
 	if (!IS_ENABLED(CONFIG_KASAN_VMALLOC))
@@ -243,26 +245,77 @@ void __init kasan_init(void)
 				    kasan_mem_to_shadow((void *)-1UL) + 1);
 
 	for_each_mem_range(i, &pa_start, &pa_end) {
-		void *start = __va(pa_start);
-		void *end = __va(pa_end);
+		/*
+		 * We move over the memory ranges, but we cannot assume that each memory range is
+		 * continuously mapped 1-to-1 from physical to virtual memory, so walk over the
+		 * pages and indentify contiguous ranges.
+		 */
+		phys_addr_t start_iter = pa_start;
+		phys_addr_t end_iter = pa_start;
+		phys_addr_t next_start = pa_start;
+		bool mapped_all = false;
+
+		pr_debug("shadow memory at %pa-%pa\n", &pa_start, &pa_end);
 
 		/* Do not attempt to shadow highmem */
-		if (pa_start >= arm_lowmem_limit) {
-			pr_info("Skip highmem block at %pa-%pa\n", &pa_start, &pa_end);
-			continue;
-		}
-		if (pa_end > arm_lowmem_limit) {
-			pr_info("Truncating shadow for memory block at %pa-%pa to lowmem region at %pa\n",
-				&pa_start, &pa_end, &arm_lowmem_limit);
-			end = __va(arm_lowmem_limit);
-		}
-		if (start >= end) {
-			pr_info("Skipping invalid memory block %pa-%pa (virtual %p-%p)\n",
-				&pa_start, &pa_end, start, end);
+		if (start_iter >= arm_lowmem_limit) {
+			pr_debug("skip highmem block at %pa-%pa\n", &pa_start, &pa_end);
 			continue;
 		}
 
-		create_mapping(start, end);
+		while (!mapped_all) {
+			void *start;
+			void *end;
+
+			end = __va(start_iter);
+
+			/* Walk pages while we are contiguous */
+			while (true) {
+				void *next_end;
+
+				/* We reached the last page of the physical memory range */
+				if (end == __va(pa_end)) {
+					mapped_all = true;
+					break;
+				}
+
+				end += PAGE_SIZE;
+				end_iter += PAGE_SIZE;
+				next_end = __va(end_iter);
+
+				if (next_end == end) {
+					/* We are contiguous */
+					continue;
+				} else {
+					next_start = end_iter;
+					break;
+				}
+			}
+			pr_debug("shadowing physical memory %08x-%08x\n", start_iter, end_iter);
+			start = __va(start_iter);
+
+			if (end_iter > arm_lowmem_limit) {
+				pr_info("truncating shadow for memory block at %pa-%pa to lowmem region at %pa\n",
+					&start_iter, &end_iter, &arm_lowmem_limit);
+				end = __va(arm_lowmem_limit);
+			}
+
+			/* Sanity check */
+			if (start >= end) {
+				pr_info("skipping invalid memory block %pa-%pa (virtual %08x-%08x)\n",
+					&start_iter, &end_iter, (u32)start, (u32)end);
+				start_iter = next_start;
+				continue;
+			}
+
+			/*
+			 * Advance this for next iteration, start_iter now points to the beginning of the
+			 * next contiguous subrange.
+			 */
+			start_iter = next_start;
+
+			create_mapping(start, end);
+		}
 	}
 
 	/*
