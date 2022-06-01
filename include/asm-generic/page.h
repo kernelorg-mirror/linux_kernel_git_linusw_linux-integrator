ed4befbf783c06820e9d4620c7fd254a36d608fe
Linus Walleij <linus.walleij@linaro.org>
2022-06-01 11:47:06 +0200

    asm-generic/page.h: Make virt_to_pfn() a static inline
    Making virt_to_pfn() a static inline taking a strongly typed
    (const void *) makes the contract of a passing a pointer of that
    type to the function explicit and exposes any misuse of the
    macro virt_to_pfn() acting polymorphic and accepting many types
    such as (void *), (unitptr_t) or (unsigned long) as arguments
    without warnings.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/asm-generic/page.h
+++ include/asm-generic/page.h
@@ -74,7 +74,10 @@ extern unsigned long memory_end;
 #define __va(x) ((void *)((unsigned long) (x)))
 #define __pa(x) ((unsigned long) (x))
 
-#define virt_to_pfn(kaddr)	(__pa(kaddr) >> PAGE_SHIFT)
+static inline unsigned long virt_to_pfn(const void *kaddr)
+{
+	return __pa(kaddr) >> PAGE_SHIFT;
+}
 #define pfn_to_virt(pfn)	__va((pfn) << PAGE_SHIFT)
 
 #define virt_to_page(addr)	pfn_to_page(virt_to_pfn(addr))
