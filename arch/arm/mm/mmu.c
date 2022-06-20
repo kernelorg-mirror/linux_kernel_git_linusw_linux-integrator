0d97f33d20bb2d46f7255a1f8509e86edbe23bc2
Linus Walleij <linus.walleij@linaro.org>
2022-06-21 00:11:57 +0200

    4G by 4G split stab
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -946,7 +946,8 @@ static void __init __create_mapping(struct mm_struct *mm, struct map_desc *md,
  */
 static void __init create_mapping(struct map_desc *md)
 {
-	if (md->virtual != vectors_base() && md->virtual < TASK_SIZE) {
+	if (md->virtual != vectors_base() &&
+	    (!IS_ENABLED(CONFIG_VMSPLIT_4G_4G) && md->virtual < TASK_SIZE)) {
 		pr_warn("BUG: not creating mapping for 0x%08llx at 0x%08lx in user region\n",
 			(long long)__pfn_to_phys((u64)md->pfn), md->virtual);
 		return;
