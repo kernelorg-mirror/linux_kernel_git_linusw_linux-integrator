764a42eab1bbf21d84c3a32570df9a390af20d43
Linus Walleij <linus.walleij@linaro.org>
2022-11-16 14:53:52 +0100

    debug prints

--- fs/exec.c
+++ fs/exec.c
@@ -916,8 +916,10 @@ static struct file *do_open_execat(int fd, struct filename *name, int flags)
 		open_exec_flags.lookup_flags |= LOOKUP_EMPTY;
 
 	file = do_filp_open(fd, name, &open_exec_flags);
-	if (IS_ERR(file))
+	if (IS_ERR(file)) {
+		pr_err("do_filp_open() failed for %s\n", name->name);
 		goto out;
+	}
 
 	/*
 	 * may_open() has already checked for this, so it should be
@@ -930,13 +932,16 @@ static struct file *do_open_execat(int fd, struct filename *name, int flags)
 		goto exit;
 
 	err = deny_write_access(file);
-	if (err)
+	if (err) {
+		pr_err("deny_write_access() failed for %s\n", name->name);
 		goto exit;
+	}
 
 	if (name->name[0] != '\0')
 		fsnotify_open(file);
 
 out:
+	pr_err("%s success on %s\n", __func__, name->name);
 	return file;
 
 exit:
@@ -1771,12 +1776,16 @@ static int exec_binprm(struct linux_binprm *bprm)
 	/* This allows 4 levels of binfmt rewrites before failing hard. */
 	for (depth = 0;; depth++) {
 		struct file *exec;
-		if (depth > 5)
+		if (depth > 5) {
+			pr_err("tried 5 levels of binfmt rewrites, giving up\n");
 			return -ELOOP;
+		}
 
 		ret = search_binary_handler(bprm);
-		if (ret < 0)
+		if (ret < 0) {
+			pr_info("search_binary_handler() failed %d\n", ret);
 			return ret;
+		}
 		if (!bprm->interpreter)
 			break;
 
@@ -1787,6 +1796,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 		allow_write_access(exec);
 		if (unlikely(bprm->have_execfd)) {
 			if (bprm->executable) {
+				pr_err("bprm->executable() was set\n");
 				fput(exec);
 				return -ENOEXEC;
 			}
@@ -1849,15 +1859,22 @@ static int bprm_execve(struct linux_binprm *bprm,
 		goto out;
 
 	retval = exec_binprm(bprm);
-	if (retval < 0)
+	if (retval < 0) {
+		pr_err("%s exec_binprm(%s) failed %d\n", __func__,
+		       filename->name,
+		       retval);
 		goto out;
+	}
 
 	/* execve succeeded */
+	pr_info("execve() succeeded\n");
 	current->fs->in_exec = 0;
 	current->in_execve = 0;
 	rseq_execve(current);
 	acct_update_integrals(current);
 	task_numa_free(current, false);
+
+	pr_info("exit from %s\n", __func__);
 	return retval;
 
 out:
@@ -2014,6 +2031,7 @@ int kernel_execve(const char *kernel_filename,
 		goto out_free;
 
 	retval = bprm_execve(bprm, fd, filename, 0);
+	pr_info("exit %s\n", __func__);
 out_free:
 	free_bprm(bprm);
 out_ret:
