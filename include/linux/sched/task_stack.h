9428ff1818f128edbb2b2da4173f6b1f9d9e765e
Pasha Tatashin <pasha.tatashin@soleen.com>
2024-03-11 16:46:37 +0000

    task_stack.h: Add stack_not_used() support for dynamic stack
    CONFIG_DEBUG_STACK_USAGE is enabled by default on most architectures.
    
    Its purpose is to determine and print the maximum stack depth on
    thread exit.
    
    The way it works, is it starts from the buttom of the stack and
    searches the first non-zero word in the stack. With dynamic stack it
    does not work very well, as it means it faults every pages in every
    stack.
    
    Instead, add a specific version of stack_not_used() for dynamic stacks
    where instead of starting from the buttom of the stack, we start from
    the last page mapped in the stack.
    
    In addition to not doing uncessary page faulting, this search is
    optimized by skipping search through zero pages.
    
    Also, because dynamic stack does not end with MAGIC_NUMBER, there is
    no need to skeep the buttom most word in the stack.
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Link: https://lore.kernel.org/20240311164638.2015063-14-pasha.tatashin@soleen.com
    [Rebased, Kasan oneliner needed preserving]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/sched/task_stack.h
+++ include/linux/sched/task_stack.h
@@ -10,6 +10,7 @@
 #include <linux/magic.h>
 #include <linux/refcount.h>
 #include <linux/kasan.h>
+#include <linux/vmalloc.h>
 
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 
@@ -110,6 +111,26 @@ static inline void dynamic_stack(struct task_struct *tsk)
 
 static inline void set_task_stack_end_magic(struct task_struct *tsk) {}
 
+#ifdef CONFIG_DEBUG_STACK_USAGE
+static inline unsigned long stack_not_used(struct task_struct *p)
+{
+	struct vm_struct *vm_area = p->stack_vm_area;
+	unsigned long alloc_size = vm_area->nr_pages << PAGE_SHIFT;
+	unsigned long stack = (unsigned long)p->stack;
+	unsigned long *n = (unsigned long *)(stack + THREAD_SIZE - alloc_size);
+
+	while (!*n)
+		n++;
+
+	return (unsigned long)n - stack;
+}
+#else
+static inline unsigned long stack_not_used(struct task_struct *p)
+{
+	return 0;
+}
+#endif /* CONFIG_DEBUG_STACK_USAGE */
+
 #else /* !CONFIG_DYNAMIC_STACK */
 
 #define task_stack_end_corrupted(task) \
@@ -124,6 +145,15 @@ static inline bool dynamic_stack_fault(struct task_struct *tsk,
 	return false;
 }
 
+#ifdef CONFIG_DEBUG_STACK_USAGE
+unsigned long stack_not_used(struct task_struct *p);
+#else
+static inline unsigned long stack_not_used(struct task_struct *p)
+{
+	return 0;
+}
+#endif
+
 #endif /* CONFIG_DYNAMIC_STACK */
 
 static inline int object_is_on_stack(const void *obj)
@@ -135,15 +165,6 @@ static inline int object_is_on_stack(const void *obj)
 }
 
 extern void thread_stack_cache_init(void);
-
-#ifdef CONFIG_DEBUG_STACK_USAGE
-unsigned long stack_not_used(struct task_struct *p);
-#else
-static inline unsigned long stack_not_used(struct task_struct *p)
-{
-	return 0;
-}
-#endif
 
 static inline int kstack_end(void *addr)
 {
