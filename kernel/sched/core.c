73c4a4effb4aea258fa709b0dab6c89140eab307
Pasha Tatashin <pasha.tatashin@soleen.com>
2024-03-11 16:46:34 +0000

    fork: Dynamic Kernel Stacks
    The core implementation of dynamic kernel stacks.
    
    Unlike traditional kernel stacks, these stack are auto-grow as they are
    used. This allows to save a significant amount of memory in the fleet
    environments. Also, potentially the default size of kernel thread can be
    increased in order to prevent stack overflows without compromising on
    the overall memory overhead.
    
    The dynamic kernel stacks interface provides two global functions:
    
    1. dynamic_stack_fault().
    Architectures that support dynamic kernel stacks, must call this function
    in order to handle the fault in the stack.
    
    It allocates and maps new pages into the stack. The pages are
    maintained in a per-cpu data structure.
    
    2. dynamic_stack()
    Must be called as a thread leaving CPU to check if the thread has
    allocated dynamic stack pages (tsk->flags & PF_DYNAMIC_STACK) is set.
    If this is the case, there are two things need to be performed:
      a. Charge the thread for the allocated stack pages.
      b. refill the per-cpu array so the next thread can also fault.
    
    Dynamic kernel threads do not support "STACK_END_MAGIC", as the last
    page is does not have to be faulted in. However, since they are based of
    vmap stacks, the guard pages always protect the dynamic kernel stacks
    from overflow.
    
    The average depth of a kernel thread depends on the workload, profiling,
    virtualization, compiler optimizations, and driver implementations.
    
    Therefore, the numbers should be tested for a specific workload. From
    my tests I found the following values on a freshly booted idling
    machines:
    
    CPU           #Cores #Stacks  Regular(kb) Dynamic(kb)
    AMD Genoa        384    5786    92576       23388
    Intel Skylake    112    3182    50912       12860
    AMD Rome         128    3401    54416       14784
    AMD Rome         256    4908    78528       20876
    Intel Haswell     72    2644    42304       10624
    
    On all machines dynamic kernel stacks take about 25% of the original
    stack memory. Only 5% of active tasks performed a stack page fault in
    their life cycles.
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Link: https://lore.kernel.org/20240311164638.2015063-11-pasha.tatashin@soleen.com
    [Rebased, used vm_area->nr_pages directly in one instance]
    [Depends on !PREEMPT_RT]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6737,6 +6737,7 @@ static void __sched notrace __schedule(int sched_mode)
 	rq = cpu_rq(cpu);
 	prev = rq->curr;
 
+	dynamic_stack(prev);
 	schedule_debug(prev, preempt);
 
 	if (sched_feat(HRTICK) || sched_feat(HRTICK_DL))
