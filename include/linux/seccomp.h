def47b081c402a8ed57444af36bb7eee07aa1955
Linus Walleij <linus.walleij@linaro.org>
2024-10-08 01:15:35 +0200

    seccomp: Stub for !HAVE_ARCH_SECCOMP_FILTER
    If we have CONFIG_SECCOMP but not CONFIG_HAVE_ARCH_SECCOMP_FILTER
    we get a compilation error:
    
    ../kernel/entry/common.c: In function 'syscall_trace_enter':
    ../kernel/entry/common.c:55:23: error: implicit declaration of function '__secure_computing' [-Werror=implicit-function-declaration]
       55 |                 ret = __secure_computing(NULL);
          |                       ^~~~~~~~~~~~~~~~~~
    
    This is because generic entry calls __secure_computing()
    unconditionally.
    
    Provide the needed stub.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/seccomp.h
+++ include/linux/seccomp.h
@@ -31,6 +31,7 @@ static inline int secure_computing(void)
 	return 0;
 }
 #else
+static inline int __secure_computing(const struct seccomp_data *sd) { return 0; }
 extern void secure_computing_strict(int this_syscall);
 #endif
 
