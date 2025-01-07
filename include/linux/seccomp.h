de759777bab7d0253b5ba09fe859b0efc8c22175
Linus Walleij <linus.walleij@linaro.org>
2025-01-07 09:41:13 +0100

    seccomp: Stub for !CONFIG_SECCOMP
    When using !CONFIG_SECCOMP with CONFIG_GENERIC_ENTRY, the
    randconfig bots found the following snag:
    
       kernel/entry/common.c: In function 'syscall_trace_enter':
    >> kernel/entry/common.c:52:23: error: implicit declaration
       of function '__secure_computing' [-Wimplicit-function-declaration]
          52 |                 ret = __secure_computing(NULL);
             |                       ^~~~~~~~~~~~~~~~~~
    
    Since generic entry calls __secure_computing() unconditionally,
    fix this by adding another static inline stub.
    
    Link: https://lore.kernel.org/oe-kbuild-all/202501061240.Fzk9qiFZ-lkp@intel.com/
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/seccomp.h
+++ include/linux/seccomp.h
@@ -58,6 +58,10 @@ static inline int secure_computing(void) { return 0; }
 static inline int __secure_computing(const struct seccomp_data *sd) { return 0; }
 #else
 static inline void secure_computing_strict(int this_syscall) { return; }
+static inline int __secure_computing(const struct seccomp_data *sd)
+{
+	return 0;
+}
 #endif
 
 static inline long prctl_get_seccomp(void)
