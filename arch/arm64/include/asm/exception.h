c5dbbed8f81c183bdc77059ec990829129c3ae7a
Linus Walleij <linus.walleij@linaro.org>
2025-06-30 15:37:54 +0200

    arm64: Support dynamic kernel stacks
    Turning on dynamic kernel stacks can save a lot of kernel
    RAM on ARM64.
    
    The approach taken here is to special-case the sync
    exceptions from the vector table. If we are handling a
    sync (and using dynamic stack), we stash (x0, x1)
    into (TPIDR_EL0, TPIDRRO_EL0) temporarily so that we
    can execute some code without using any stack at all.
    
    Using TPIDR_EL0 and TPIDRRO_EL0 is safe because a
    previous patch confine thes to EL0 (userspace) and
    syncs on the stack will only happen from EL1 and we
    only concern ourselves with el1h_64* handlers here.
    
    We define a special sync stack that is only used
    when handling sync calls. and we switch to this stack
    immediately in the exception ventry handler if this
    was a DABT exception on the task stack, without
    saving a single value onto the task stack. By using
    a special sync stack we avoid any risk of recursive
    data aborts.
    
    The ventry assembly check if this sync exception was
    
    1) A data abort (DABT)
    2) On the ordinary process stack (current->stack)
    
    If it was not, we branch out and handle the sync as usual,
    on the ordinary stack.
    
    Else we:
    
    3) Switch SP to the special sync stack
    4) Save the old SP at the bottom of the special sync stack
    5) Proceed to handle the abort on this stack
    6) In the C portion of the sync handler (el1h_64_sync_handler)
       we detect the condition again and act accordingly
    7) If this was indeed a data abort on the task stack,
       we call do_stack_abort() which in turn calls
       dynamic_stack_fault() to latch in a new physical
       page to the stack, all while running on the temporary
       sync stack.
    8) We then return from the exception restoring
       SP to what it used to be before the DABT on stack sync
       exception.
    
    The last step is achievend by passing a return value from
    el1h_64_sync_handler() that will be 1 if we were handling
    a DABT on the stack.
    
    9) A special ret_to_kernel_el1_sync is used
    10) This uses a parameterized version of the kernel_exit
      macro that will not only restore the registers but also
      the old SP before the DABT on the stack.
    
    Restoring SP to the old value requires a bit of trickery
    which involves using the newly allocated stack page to
    temporarily store x0 and x1.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm64/include/asm/exception.h
+++ arch/arm64/include/asm/exception.h
@@ -28,11 +28,13 @@ static inline unsigned long disr_to_esr(u64 disr)
 
 asmlinkage void __noreturn handle_bad_stack(struct pt_regs *regs);
 
+asmlinkage void el1t_64_sync_stack_dabt_handler(struct pt_regs *regs);
 asmlinkage void el1t_64_sync_handler(struct pt_regs *regs);
 asmlinkage void el1t_64_irq_handler(struct pt_regs *regs);
 asmlinkage void el1t_64_fiq_handler(struct pt_regs *regs);
 asmlinkage void el1t_64_error_handler(struct pt_regs *regs);
 
+asmlinkage void el1h_64_sync_stack_dabt_handler(struct pt_regs *regs);
 asmlinkage void el1h_64_sync_handler(struct pt_regs *regs);
 asmlinkage void el1h_64_irq_handler(struct pt_regs *regs);
 asmlinkage void el1h_64_fiq_handler(struct pt_regs *regs);
@@ -52,6 +54,7 @@ asmlinkage void call_on_irq_stack(struct pt_regs *regs,
 				  void (*func)(struct pt_regs *));
 asmlinkage void asm_exit_to_user_mode(struct pt_regs *regs);
 
+void do_stack_abort(unsigned long far, struct pt_regs *regs);
 void do_mem_abort(unsigned long far, unsigned long esr, struct pt_regs *regs);
 void do_el0_undef(struct pt_regs *regs, unsigned long esr);
 void do_el1_undef(struct pt_regs *regs, unsigned long esr);
--- arch/arm64/kernel/entry-common.c
+++ arch/arm64/kernel/entry-common.c
@@ -288,6 +288,7 @@ static void debug_exception_exit(struct pt_regs *regs)
 }
 NOKPROBE_SYMBOL(debug_exception_exit);
 
+UNHANDLED(el1t, 64, sync_stack_dabt)
 UNHANDLED(el1t, 64, sync)
 UNHANDLED(el1t, 64, irq)
 UNHANDLED(el1t, 64, fiq)
@@ -426,6 +427,21 @@ static void noinstr el1_fpac(struct pt_regs *regs, unsigned long esr)
 	local_daif_mask();
 	exit_to_kernel_mode(regs, state);
 }
+
+#ifdef CONFIG_DYNAMIC_STACK
+asmlinkage void noinstr el1h_64_sync_stack_dabt_handler(struct pt_regs *regs)
+{
+	unsigned long far = read_sysreg(far_el1);
+	unsigned long addr = untagged_addr(far);
+	unsigned long stack = (unsigned long)current->stack;
+
+	/* We ran over the top of the stack! */
+	if (addr >= (stack - PAGE_SIZE) && addr < stack)
+		handle_bad_stack(regs);
+
+	do_stack_abort(far, regs);
+}
+#endif
 
 asmlinkage void noinstr el1h_64_sync_handler(struct pt_regs *regs)
 {
