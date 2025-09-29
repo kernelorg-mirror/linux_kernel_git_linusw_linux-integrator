f0fbdb6bd9d75787730bcc9bc241311b112ccb6a
Linus Walleij <linus.walleij@linaro.org>
2025-09-29 14:54:27 +0200

    pinctrl: bcm: Rename bcm4908 to bcmbca
    The basic structure and the hardware of all BCA SoCs are the
    same as for BCM4908 so rename all symbols to BCMBCA.
    
    This makes for a good base to isolate/parameterize and split
    out BCM4908 and add other SoCs.
    
    The driver is unconditionally selected for all ARCH_BCMBCA
    anyway so renaming the symbol should be pretty painless.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/bcm/pinctrl-bcmbca.c
+++ drivers/pinctrl/bcm/pinctrl-bcmbca.c
@@ -17,14 +17,14 @@
 
 #define BCM4908_NUM_PINS			86
 
-#define BCM4908_TEST_PORT_BLOCK_EN_LSB			0x00
-#define BCM4908_TEST_PORT_BLOCK_DATA_MSB		0x04
-#define BCM4908_TEST_PORT_BLOCK_DATA_LSB		0x08
-#define  BCM4908_TEST_PORT_LSB_PINMUX_DATA_SHIFT	12
-#define BCM4908_TEST_PORT_COMMAND			0x0c
-#define  BCM4908_TEST_PORT_CMD_LOAD_MUX_REG		0x00000021
+#define BCMBCA_TEST_PORT_BLOCK_EN_LSB			0x00
+#define BCMBCA_TEST_PORT_BLOCK_DATA_MSB			0x04
+#define BCMBCA_TEST_PORT_BLOCK_DATA_LSB			0x08
+#define  BCMBCA_TEST_PORT_LSB_PINMUX_DATA_SHIFT		12
+#define BCMBCA_TEST_PORT_COMMAND			0x0c
+#define  BCMBCA_TEST_PORT_CMD_LOAD_MUX_REG		0x00000021
 
-struct bcm4908_pinctrl {
+struct bcmbca_pinctrl {
 	struct device *dev;
 	void __iomem *base;
 	struct mutex mutex;
@@ -36,184 +36,184 @@ struct bcm4908_pinctrl {
  * Groups
  */
 
-struct bcm4908_pinctrl_pin_setup {
+struct bcmbca_pinctrl_pin_setup {
 	unsigned int number;
 	unsigned int function;
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_0_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_0_pins_a[] = {
 	{ 0, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_1_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_1_pins_a[] = {
 	{ 1, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_2_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_2_pins_a[] = {
 	{ 2, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_3_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_3_pins_a[] = {
 	{ 3, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_4_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_4_pins_a[] = {
 	{ 4, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_5_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_5_pins_a[] = {
 	{ 5, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_6_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_6_pins_a[] = {
 	{ 6, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_7_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_7_pins_a[] = {
 	{ 7, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_8_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_8_pins_a[] = {
 	{ 8, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_9_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_9_pins_a[] = {
 	{ 9, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_10_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_10_pins_a[] = {
 	{ 10, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_11_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_11_pins_a[] = {
 	{ 11, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_12_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_12_pins_a[] = {
 	{ 12, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_13_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_13_pins_a[] = {
 	{ 13, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_14_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_14_pins_a[] = {
 	{ 14, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_15_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_15_pins_a[] = {
 	{ 15, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_16_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_16_pins_a[] = {
 	{ 16, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_17_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_17_pins_a[] = {
 	{ 17, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_18_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_18_pins_a[] = {
 	{ 18, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_19_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_19_pins_a[] = {
 	{ 19, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_20_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_20_pins_a[] = {
 	{ 20, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_21_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_21_pins_a[] = {
 	{ 21, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_22_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_22_pins_a[] = {
 	{ 22, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_23_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_23_pins_a[] = {
 	{ 23, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_24_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_24_pins_a[] = {
 	{ 24, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_25_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_25_pins_a[] = {
 	{ 25, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_26_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_26_pins_a[] = {
 	{ 26, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_27_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_27_pins_a[] = {
 	{ 27, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_28_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_28_pins_a[] = {
 	{ 28, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_29_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_29_pins_a[] = {
 	{ 29, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_30_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_30_pins_a[] = {
 	{ 30, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_31_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup led_31_pins_a[] = {
 	{ 31, 3 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_10_pins_b[] = {
+static const struct bcmbca_pinctrl_pin_setup led_10_pins_b[] = {
 	{ 8, 2 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_11_pins_b[] = {
+static const struct bcmbca_pinctrl_pin_setup led_11_pins_b[] = {
 	{ 9, 2 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_12_pins_b[] = {
+static const struct bcmbca_pinctrl_pin_setup led_12_pins_b[] = {
 	{ 0, 2 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_13_pins_b[] = {
+static const struct bcmbca_pinctrl_pin_setup led_13_pins_b[] = {
 	{ 1, 2 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup led_31_pins_b[] = {
+static const struct bcmbca_pinctrl_pin_setup led_31_pins_b[] = {
 	{ 30, 2 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup hs_uart_pins[] = {
+static const struct bcmbca_pinctrl_pin_setup hs_uart_pins[] = {
 	{ 10, 0 },	/* CTS */
 	{ 11, 0 },	/* RTS */
 	{ 12, 0 },	/* RXD */
 	{ 13, 0 },	/* TXD */
 };
 
-static const struct bcm4908_pinctrl_pin_setup i2c_pins_a[] = {
+static const struct bcmbca_pinctrl_pin_setup i2c_pins_a[] = {
 	{ 18, 0 },	/* SDA */
 	{ 19, 0 },	/* SCL */
 };
 
-static const struct bcm4908_pinctrl_pin_setup i2c_pins_b[] = {
+static const struct bcmbca_pinctrl_pin_setup i2c_pins_b[] = {
 	{ 22, 0 },	/* SDA */
 	{ 23, 0 },	/* SCL */
 };
 
-static const struct bcm4908_pinctrl_pin_setup i2s_pins[] = {
+static const struct bcmbca_pinctrl_pin_setup i2s_pins[] = {
 	{ 27, 0 },	/* MCLK */
 	{ 28, 0 },	/* LRCK */
 	{ 29, 0 },	/* SDATA */
 	{ 30, 0 },	/* SCLK */
 };
 
-static const struct bcm4908_pinctrl_pin_setup nand_ctrl_pins[] = {
+static const struct bcmbca_pinctrl_pin_setup nand_ctrl_pins[] = {
 	{ 32, 0 },
 	{ 33, 0 },
 	{ 34, 0 },
@@ -223,7 +223,7 @@ static const struct bcm4908_pinctrl_pin_setup nand_ctrl_pins[] = {
 	{ 56, 1 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup nand_data_pins[] = {
+static const struct bcmbca_pinctrl_pin_setup nand_data_pins[] = {
 	{ 35, 0 },
 	{ 36, 0 },
 	{ 37, 0 },
@@ -234,28 +234,28 @@ static const struct bcm4908_pinctrl_pin_setup nand_data_pins[] = {
 	{ 42, 0 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup emmc_ctrl_pins[] = {
+static const struct bcmbca_pinctrl_pin_setup emmc_ctrl_pins[] = {
 	{ 46, 0 },
 	{ 47, 0 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup usb0_pwr_pins[] = {
+static const struct bcmbca_pinctrl_pin_setup usb0_pwr_pins[] = {
 	{ 63, 0 },
 	{ 64, 0 },
 };
 
-static const struct bcm4908_pinctrl_pin_setup usb1_pwr_pins[] = {
+static const struct bcmbca_pinctrl_pin_setup usb1_pwr_pins[] = {
 	{ 66, 0 },
 	{ 67, 0 },
 };
 
-struct bcm4908_pinctrl_grp {
+struct bcmbca_pinctrl_grp {
 	const char *name;
-	const struct bcm4908_pinctrl_pin_setup *pins;
+	const struct bcmbca_pinctrl_pin_setup *pins;
 	const unsigned int num_pins;
 };
 
-static const struct bcm4908_pinctrl_grp bcm4908_pinctrl_grps[] = {
+static const struct bcmbca_pinctrl_grp bcm4908_pinctrl_grps[] = {
 	{ "led_0_grp_a", led_0_pins_a, ARRAY_SIZE(led_0_pins_a) },
 	{ "led_1_grp_a", led_1_pins_a, ARRAY_SIZE(led_1_pins_a) },
 	{ "led_2_grp_a", led_2_pins_a, ARRAY_SIZE(led_2_pins_a) },
@@ -308,7 +308,7 @@ static const struct bcm4908_pinctrl_grp bcm4908_pinctrl_grps[] = {
  * Functions
  */
 
-struct bcm4908_pinctrl_function {
+struct bcmbca_pinctrl_function {
 	const char *name;
 	const char * const *groups;
 	const unsigned int num_groups;
@@ -355,7 +355,7 @@ static const char * const emmc_ctrl_groups[] = { "emmc_ctrl_grp" };
 static const char * const usb0_pwr_groups[] = { "usb0_pwr_grp" };
 static const char * const usb1_pwr_groups[] = { "usb1_pwr_grp" };
 
-static const struct bcm4908_pinctrl_function bcm4908_pinctrl_functions[] = {
+static const struct bcmbca_pinctrl_function bcm4908_pinctrl_functions[] = {
 	{ "led_0", led_0_groups, ARRAY_SIZE(led_0_groups) },
 	{ "led_1", led_1_groups, ARRAY_SIZE(led_1_groups) },
 	{ "led_2", led_2_groups, ARRAY_SIZE(led_2_groups) },
@@ -402,7 +402,7 @@ static const struct bcm4908_pinctrl_function bcm4908_pinctrl_functions[] = {
  * Groups code
  */
 
-static const struct pinctrl_ops bcm4908_pinctrl_ops = {
+static const struct pinctrl_ops bcmbca_pinctrl_ops = {
 	.get_groups_count = pinctrl_generic_get_group_count,
 	.get_group_name = pinctrl_generic_get_group_name,
 	.get_group_pins = pinctrl_generic_get_group_pins,
@@ -414,12 +414,12 @@ static const struct pinctrl_ops bcm4908_pinctrl_ops = {
  * Functions code
  */
 
-static int bcm4908_pinctrl_set_mux(struct pinctrl_dev *pctrl_dev,
+static int bcmbca_pinctrl_set_mux(struct pinctrl_dev *pctrl_dev,
 			      unsigned int func_selector,
 			      unsigned int group_selector)
 {
-	struct bcm4908_pinctrl *bcm4908_pinctrl = pinctrl_dev_get_drvdata(pctrl_dev);
-	const struct bcm4908_pinctrl_grp *group;
+	struct bcmbca_pinctrl *bcmbca_pinctrl = pinctrl_dev_get_drvdata(pctrl_dev);
+	const struct bcmbca_pinctrl_grp *group;
 	struct group_desc *group_desc;
 	int i;
 
@@ -428,71 +428,71 @@ static int bcm4908_pinctrl_set_mux(struct pinctrl_dev *pctrl_dev,
 		return -EINVAL;
 	group = group_desc->data;
 
-	mutex_lock(&bcm4908_pinctrl->mutex);
+	mutex_lock(&bcmbca_pinctrl->mutex);
 	for (i = 0; i < group->num_pins; i++) {
 		u32 lsb = 0;
 
 		lsb |= group->pins[i].number;
-		lsb |= group->pins[i].function << BCM4908_TEST_PORT_LSB_PINMUX_DATA_SHIFT;
+		lsb |= group->pins[i].function << BCMBCA_TEST_PORT_LSB_PINMUX_DATA_SHIFT;
 
-		writel(0x0, bcm4908_pinctrl->base + BCM4908_TEST_PORT_BLOCK_DATA_MSB);
-		writel(lsb, bcm4908_pinctrl->base + BCM4908_TEST_PORT_BLOCK_DATA_LSB);
-		writel(BCM4908_TEST_PORT_CMD_LOAD_MUX_REG,
-		       bcm4908_pinctrl->base + BCM4908_TEST_PORT_COMMAND);
+		writel(0x0, bcmbca_pinctrl->base + BCMBCA_TEST_PORT_BLOCK_DATA_MSB);
+		writel(lsb, bcmbca_pinctrl->base + BCMBCA_TEST_PORT_BLOCK_DATA_LSB);
+		writel(BCMBCA_TEST_PORT_CMD_LOAD_MUX_REG,
+		       bcmbca_pinctrl->base + BCMBCA_TEST_PORT_COMMAND);
 	}
-	mutex_unlock(&bcm4908_pinctrl->mutex);
+	mutex_unlock(&bcmbca_pinctrl->mutex);
 
 	return 0;
 }
 
-static const struct pinmux_ops bcm4908_pinctrl_pmxops = {
+static const struct pinmux_ops bcmbca_pinctrl_pmxops = {
 	.get_functions_count = pinmux_generic_get_function_count,
 	.get_function_name = pinmux_generic_get_function_name,
 	.get_function_groups = pinmux_generic_get_function_groups,
-	.set_mux = bcm4908_pinctrl_set_mux,
+	.set_mux = bcmbca_pinctrl_set_mux,
 };
 
 /*
  * Controller code
  */
 
-static const struct pinctrl_desc bcm4908_pinctrl_desc = {
-	.name = "bcm4908-pinctrl",
-	.pctlops = &bcm4908_pinctrl_ops,
-	.pmxops = &bcm4908_pinctrl_pmxops,
+static const struct pinctrl_desc bcmbca_pinctrl_desc = {
+	.name = "bcmbca-pinctrl",
+	.pctlops = &bcmbca_pinctrl_ops,
+	.pmxops = &bcmbca_pinctrl_pmxops,
 };
 
-static const struct of_device_id bcm4908_pinctrl_of_match_table[] = {
+static const struct of_device_id bcmbca_pinctrl_of_match_table[] = {
 	{ .compatible = "brcm,bcm4908-pinctrl", },
 	{ }
 };
 
-static int bcm4908_pinctrl_probe(struct platform_device *pdev)
+static int bcmbca_pinctrl_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct bcm4908_pinctrl *bcm4908_pinctrl;
+	struct bcmbca_pinctrl *bcmbca_pinctrl;
 	struct pinctrl_desc *pctldesc;
 	struct pinctrl_pin_desc *pins;
 	char **pin_names;
 	int i;
 
-	bcm4908_pinctrl = devm_kzalloc(dev, sizeof(*bcm4908_pinctrl), GFP_KERNEL);
-	if (!bcm4908_pinctrl)
+	bcmbca_pinctrl = devm_kzalloc(dev, sizeof(*bcmbca_pinctrl), GFP_KERNEL);
+	if (!bcmbca_pinctrl)
 		return -ENOMEM;
-	pctldesc = &bcm4908_pinctrl->pctldesc;
-	platform_set_drvdata(pdev, bcm4908_pinctrl);
+	pctldesc = &bcmbca_pinctrl->pctldesc;
+	platform_set_drvdata(pdev, bcmbca_pinctrl);
 
 	/* Set basic properties */
 
-	bcm4908_pinctrl->dev = dev;
+	bcmbca_pinctrl->dev = dev;
 
-	bcm4908_pinctrl->base = devm_platform_ioremap_resource(pdev, 0);
-	if (IS_ERR(bcm4908_pinctrl->base))
-		return PTR_ERR(bcm4908_pinctrl->base);
+	bcmbca_pinctrl->base = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(bcmbca_pinctrl->base))
+		return PTR_ERR(bcmbca_pinctrl->base);
 
-	mutex_init(&bcm4908_pinctrl->mutex);
+	mutex_init(&bcmbca_pinctrl->mutex);
 
-	memcpy(pctldesc, &bcm4908_pinctrl_desc, sizeof(*pctldesc));
+	memcpy(pctldesc, &bcmbca_pinctrl_desc, sizeof(*pctldesc));
 
 	/* Set pinctrl properties */
 
@@ -512,15 +512,15 @@ static int bcm4908_pinctrl_probe(struct platform_device *pdev)
 
 	/* Register */
 
-	bcm4908_pinctrl->pctldev = devm_pinctrl_register(dev, pctldesc, bcm4908_pinctrl);
-	if (IS_ERR(bcm4908_pinctrl->pctldev))
-		return dev_err_probe(dev, PTR_ERR(bcm4908_pinctrl->pctldev),
+	bcmbca_pinctrl->pctldev = devm_pinctrl_register(dev, pctldesc, bcmbca_pinctrl);
+	if (IS_ERR(bcmbca_pinctrl->pctldev))
+		return dev_err_probe(dev, PTR_ERR(bcmbca_pinctrl->pctldev),
 				     "Failed to register pinctrl\n");
 
 	/* Groups */
 
 	for (i = 0; i < ARRAY_SIZE(bcm4908_pinctrl_grps); i++) {
-		const struct bcm4908_pinctrl_grp *group = &bcm4908_pinctrl_grps[i];
+		const struct bcmbca_pinctrl_grp *group = &bcm4908_pinctrl_grps[i];
 		int *pins;
 		int j;
 
@@ -530,16 +530,16 @@ static int bcm4908_pinctrl_probe(struct platform_device *pdev)
 		for (j = 0; j < group->num_pins; j++)
 			pins[j] = group->pins[j].number;
 
-		pinctrl_generic_add_group(bcm4908_pinctrl->pctldev, group->name,
+		pinctrl_generic_add_group(bcmbca_pinctrl->pctldev, group->name,
 					  pins, group->num_pins, (void *)group);
 	}
 
 	/* Functions */
 
 	for (i = 0; i < ARRAY_SIZE(bcm4908_pinctrl_functions); i++) {
-		const struct bcm4908_pinctrl_function *function = &bcm4908_pinctrl_functions[i];
+		const struct bcmbca_pinctrl_function *function = &bcm4908_pinctrl_functions[i];
 
-		pinmux_generic_add_function(bcm4908_pinctrl->pctldev,
+		pinmux_generic_add_function(bcmbca_pinctrl->pctldev,
 					    function->name,
 					    function->groups,
 					    function->num_groups, NULL);
@@ -548,17 +548,17 @@ static int bcm4908_pinctrl_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static struct platform_driver bcm4908_pinctrl_driver = {
-	.probe = bcm4908_pinctrl_probe,
+static struct platform_driver bcmbca_pinctrl_driver = {
+	.probe = bcmbca_pinctrl_probe,
 	.driver = {
-		.name = "bcm4908-pinctrl",
-		.of_match_table = bcm4908_pinctrl_of_match_table,
+		.name = "bcmbca-pinctrl",
+		.of_match_table = bcmbca_pinctrl_of_match_table,
 	},
 };
 
-module_platform_driver(bcm4908_pinctrl_driver);
+module_platform_driver(bcmbca_pinctrl_driver);
 
 MODULE_AUTHOR("Rafał Miłecki");
-MODULE_DESCRIPTION("Broadcom BCM4908 pinmux driver");
+MODULE_DESCRIPTION("Broadcom BCMBCA pinmux driver");
 MODULE_LICENSE("GPL v2");
-MODULE_DEVICE_TABLE(of, bcm4908_pinctrl_of_match_table);
+MODULE_DEVICE_TABLE(of, bcmbca_pinctrl_of_match_table);
