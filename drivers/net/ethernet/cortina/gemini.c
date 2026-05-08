33d8b475de791e0a7d02d11d7ebce7994295d39a
Linus Walleij <linusw@kernel.org>
2026-05-08 23:49:48 +0200

    net: ethernet: cortina: Fix budget accounting
    The gmac_rx() function should return the number of packets received,
    not the remaining budget, as it currently does.
    
    In gmac_napi_poll(), this return value is assigned to received,
    and if received < budget, the driver calls
    napi_complete_done(napi, received).
    
    Fix this by adding a new local variable for the number of received
    packets in gmac_rx() and return that instead.
    
    This was found by Sashiko during normal patch review.
    
    Fixes: 4d5ae32f5e1e ("net: ethernet: Add a driver for Gemini gigabit ethernet")
    Link: https://sashiko.dev/#/patchset/20260505-gemini-ethernet-fix-v2-1-997c31d06079%40kernel.org
    Signed-off-by: Linus Walleij <linusw@kernel.org>

--- drivers/net/ethernet/cortina/gemini.c
+++ drivers/net/ethernet/cortina/gemini.c
@@ -1450,6 +1450,7 @@ static unsigned int gmac_rx(struct net_device *netdev, unsigned int budget)
 	unsigned int frame_len, frag_len;
 	struct gmac_rxdesc *rx = NULL;
 	struct gmac_queue_page *gpage;
+	unsigned int received = 0;
 	union gmac_rxdesc_0 word0;
 	union gmac_rxdesc_1 word1;
 	union gmac_rxdesc_3 word3;
@@ -1545,7 +1546,8 @@ static unsigned int gmac_rx(struct net_device *netdev, unsigned int budget)
 			napi_gro_frags(&port->napi);
 			skb = NULL;
 			frag_nr = 0;
-			--budget;
+			budget--;
+			received++;
 		}
 		continue;
 
@@ -1565,7 +1567,7 @@ err_drop:
 	port->rx_skb = skb;
 	port->rx_frag_nr = frag_nr;
 	writew(r, ptr_reg);
-	return budget;
+	return received;
 }
 
 static int gmac_napi_poll(struct napi_struct *napi, int budget)
